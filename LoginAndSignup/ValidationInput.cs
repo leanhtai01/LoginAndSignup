493a8cba2631830c1527cfe781afccbe4ff3baec
leanhtai01 <leanhtai01@gmail.com>
2020-11-19 09:10:33 +0700

    add utility for verify user's input

---
+++ LoginAndSignup/ValidationInput.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LoginAndSignup
+{
+    public class ValidationInput
+    {
+        /// <summary>
+        /// Check whether input string satisfy a pattern
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <param name="pattern"></param>
+        /// <returns></returns>
+        public bool IsValid(string inputString, string pattern)
+        {
+            Regex regex = new Regex(pattern);
+
+            return regex.IsMatch(inputString);
+        }
+    }
+}
